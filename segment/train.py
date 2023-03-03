8997abe62dac45f43013f39497c137f13fa5f3e7
Yijun Jiang <yijun1121@gmail.com>
2023-03-02 23:55:32 -0800

    add verbose

--- segment/train.py
+++ segment/train.py
@@ -284,6 +284,9 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
             # callbacks.run('on_train_batch_start')
             ni = i + nb * epoch  # number integrated batches (since train start)
             imgs = imgs.to(device, non_blocking=True).float() / 255  # uint8 to float32, 0-255 to 0.0-1.0
+            
+            if i==0 and epoch==0:
+                print(imgs.shape)
 
             # Warmup
             if ni <= nw:
