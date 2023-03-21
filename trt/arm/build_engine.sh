28f4e364e384784a9e38e2315ac3fdb6a6ae3a8e
Yijun Jiang <yijun1121@gmail.com>
2023-03-21 12:55:24 -0700

    update trt scripts

--- trt/arm/build_engine.sh
+++ trt/arm/build_engine.sh
@@ -7,5 +7,9 @@ python3 -m yolov5.export --weights $MODEL_PATH/best.pt \
     --imgsz $IM_H $IM_W  --include engine --half --device 0
 
 # validation
-python3 -m yolov5.trt.infer_trt --engine $MODEL_PATH/best.engine \
-    --imsz $IM_H $IM_W --path_imgs /app/images --path_out /app/validation
+if [ -d /app/images ]; then
+    python3 -m yolov5.trt.infer_trt --engine $MODEL_PATH/best.engine \
+        --imsz $IM_H $IM_W --path_imgs /app/images --path_out /app/validation
+else
+    echo "Testing images are not found in /app/images, skip validation"
+fi
